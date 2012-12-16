8b84f1ac5be18b7b2354a7328ef244caa0542207
Eddie Massey III <eem33@case.edu>
2012-12-16 09:51:58 -0500

    Added transform publisher

---
+++ TransformConfiguration/src/tfBroadcaster.cpp
@@ -0,0 +1,45 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char** argv)
+{
+
+  ros::init(argc, argv, "tf_publisher");
+  ros::NodeHandle n;
+
+  ros::Rate r(100);
+  
+  tf::TransformBroadcaster broadcaster;
+  
+  while(n.ok())
+    {
+
+      //Transform From Robot Frame to Camera Frame
+      broadcaster.sendTransform(
+				tf::StampedTransform(
+						     tf::Transform(tf::Quaternion(0,0,0,1), tf::Vector3(0.1,0.0,0.2)),
+						     ros::Time::now(),"base_link", "base_laser"));
+
+      //Transform from Robot to Gripper
+      //Transform for Odom
+      r.sleep();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
